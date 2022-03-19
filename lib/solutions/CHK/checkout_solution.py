2a5f3953a4178a98709c835252884ef3a9af6f51
sbx_user1051 <sbx_user1051@169.254.0.181>
2022-03-19 16:03:35 +0000

    Sat Mar 19 16:03:35 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,25 @@
 
 
 class Item():
-    def __init__(self, key, price, special_offer):
-        self.key = key
+    def __init__(self, sku, price, special_offer):
+        self.sku = sku
         self.price = price
         self.special_offer = special_offer
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    items = [
+        Item("A", 50, (3, 130)),
+        Item("B", 30, (2, 45)),
+        Item("C", 20, None),
+        Item("D", 15, None)
+    ]
+
+    for sku in skus:
+        if sku not in [x.sku for x in items]:
+            return -1
+
+    return 0
+
 
