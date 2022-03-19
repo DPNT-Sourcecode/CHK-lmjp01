64bbb09f48099014b2a991d47cc071fc72b7cb9c
sbx_user1051 <sbx_user1051@169.254.34.165>
2022-03-19 23:54:25 +0000

    Sat Mar 19 23:54:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,6 +14,9 @@ class VolumeDiscount():
         self.volume = volume
         self.price = price
 
+    def get_price_per_item(self):
+        return self.price / self.volume
+
 
 class FreebieOffer():
     def __init__(self, count, freebies):
@@ -28,7 +31,8 @@ def checkout(skus):
         Item("A", 50, [VolumeDiscount(3, 130), VolumeDiscount(5, 200)], None),
         Item("B", 30, [VolumeDiscount(2, 45)], None),
         Item("C", 20, None, None),
-        Item("D", 15, None, [FreebieOffer(2, {"B": 1})])
+        Item("D", 15, None, None),
+        Item("E", 40, None, [FreebieOffer(2, {"B": 1})])
     ]
 
     sku_count = defaultdict(int)
@@ -40,6 +44,18 @@ def checkout(skus):
 
     total_price = 0
 
+    """
+    for item
+    calc maximum volume discount savings
+    calc freebies possible
+    calc price and save
+    invert, calc price, compare
+    """
+
+    for item in items:
+
+
     return total_price
+
 
 
