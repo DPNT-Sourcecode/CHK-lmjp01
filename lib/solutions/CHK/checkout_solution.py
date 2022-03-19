bf273df067952ce53f9c6050274167ea16769567
sbx_user1051 <sbx_user1051@169.254.0.181>
2022-03-19 16:00:35 +0000

    Sat Mar 19 16:00:35 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,13 @@
 
 
+class Item():
+    def __init__(self, key, price, special_offer):
+        self.key = key
+        self.price = price
+        self.special_offer = special_offer
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     raise NotImplementedError()
+
