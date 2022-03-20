7247383549952da17f464600a58fed7949b3d95b
sbx_user1051 <sbx_user1051@169.254.2.69>
2022-03-20 21:53:29 +0000

    Sun Mar 20 21:53:29 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -80,11 +80,11 @@ def build_items(table_string):
         freebie_offers = []
         for offer_string in [x.strip() for x in offers_string.split(', ')]:
             offer_string_fields = offer_string.split()
-            if ' for ' in offer_string:
+            if offer_string_fields[1] == "for":
                 volume_count = int(offer_string_fields[0].split(sku)[0])
                 volume_price = int(offer_string_fields[2])
                 volume_discounts.append(VolumeDiscount(volume_count, volume_price))
-            if ' get ' in offer_string:
+            if offer_string_fields[1] == "get":
                 freebie_count = int(offer_string_fields[0].split(sku)[0])
                 freebie_sku = offer_string_fields[3]
                 if freebie_sku == sku:
@@ -96,36 +96,36 @@ def build_items(table_string):
 
 def build_items_wrapper():
     table_string = """
-        +------+-------+------------------------+
-        | Item | Price | Special offers         |
-        +------+-------+------------------------+
-        | A    | 50    | 3A for 130, 5A for 200 |
-        | B    | 30    | 2B for 45              |
-        | C    | 20    |                        |
-        | D    | 15    |                        |
-        | E    | 40    | 2E get one B free      |
-        | F    | 10    | 2F get one F free      |
-        | G    | 20    |                        |
-        | H    | 10    | 5H for 45, 10H for 80  |
-        | I    | 35    |                        |
-        | J    | 60    |                        |
-        | K    | 80    | 2K for 150             |
-        | L    | 90    |                        |
-        | M    | 15    |                        |
-        | N    | 40    | 3N get one M free      |
-        | O    | 10    |                        |
-        | P    | 50    | 5P for 200             |
-        | Q    | 30    | 3Q for 80              |
-        | R    | 50    | 3R get one Q free      |
-        | S    | 30    |                        |
-        | T    | 20    |                        |
-        | U    | 40    | 3U get one U free      |
-        | V    | 50    | 2V for 90, 3V for 130  |
-        | W    | 20    |                        |
-        | X    | 90    |                        |
-        | Y    | 10    |                        |
-        | Z    | 50    |                        |
-        +------+-------+------------------------+
+        +------+-------+---------------------------------+
+        | Item | Price | Special offers                  |
+        +------+-------+---------------------------------+
+        | A    | 50    | 3A for 130, 5A for 200          |
+        | B    | 30    | 2B for 45                       |
+        | C    | 20    |                                 |
+        | D    | 15    |                                 |
+        | E    | 40    | 2E get one B free               |
+        | F    | 10    | 2F get one F free               |
+        | G    | 20    |                                 |
+        | H    | 10    | 5H for 45, 10H for 80           |
+        | I    | 35    |                                 |
+        | J    | 60    |                                 |
+        | K    | 70    | 2K for 120                      |
+        | L    | 90    |                                 |
+        | M    | 15    |                                 |
+        | N    | 40    | 3N get one M free               |
+        | O    | 10    |                                 |
+        | P    | 50    | 5P for 200                      |
+        | Q    | 30    | 3Q for 80                       |
+        | R    | 50    | 3R get one Q free               |
+        | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | U    | 40    | 3U get one U free               |
+        | V    | 50    | 2V for 90, 3V for 130           |
+        | W    | 20    |                                 |
+        | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        +------+-------+---------------------------------+
     """
 
     return build_items(table_string)
@@ -149,4 +149,5 @@ def checkout(skus):
     price_freebies_first = calc_price_freebies_first(items, copy.deepcopy(sku_count))
 
     return min(price_volume_first, price_freebies_first)
+
 
