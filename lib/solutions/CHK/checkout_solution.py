d0d63b24211debd3d3f0f6c9a2a9fbdb9f4b1f61
sbx_user1051 <sbx_user1051@169.254.34.165>
2022-03-20 00:24:26 +0000

    Sun Mar 20 00:24:26 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -36,18 +36,23 @@ def calc_max_volume_discount(item, sku_count):
     return price
 
 
-def calc_eligible_freebies(items, sku_count):
-    for item in items:
-        if item.freebie_offers:
-            pass
+def calc_eligible_freebies(item, sku_count):
+    if item.freebie_offers:
+        for freebie_offer in item.freebie_offers:
+            times_applied = sku_count[item.sku] // freebie_offer.count
+            for freebie_sku in freebie_offer.freebies.keys():
+                sku_count[freebie_sku] -= times_applied * freebie_offer.freebies[freebie_sku]
 
 
 def calc_price_volume_first(items, sku_count):
     total_price = 0
     for item in items:
         max_volume_discount = calc_max_volume_discount(item, sku_count)
-        calc_eligible_freebies(items, sku_count)
-        total_price += max_volume_discount + (sku_count[item.sku] * item.price)
+        total_price += max_volume_discount
+    for item in items:
+        calc_eligible_freebies(item, sku_count)
+    for item in items:
+        total_price += sku_count[item.sku] * item.price
     return total_price
 
 
@@ -81,6 +86,7 @@ def checkout(skus):
     price_freebies_first = 0
 
     return min(price_volume_first, price_freebies_first)
+
 
 
 
