83cd7835a7472015ff24811fcd40a5b88821687c
sbx_user1051 <sbx_user1051@169.254.91.181>
2022-03-20 16:47:33 +0000

    Sun Mar 20 16:47:33 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -97,39 +97,9 @@ def build_items(table_string):
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    table_string = """+------+-------+------------------------+
-| Item | Price | Special offers         |
-+------+-------+------------------------+
-| A    | 50    | 3A for 130, 5A for 200 |
-| B    | 30    | 2B for 45              |
-| C    | 20    |                        |
-| D    | 15    |                        |
-| E    | 40    | 2E get one B free      |
-| F    | 10    | 2F get one F free      |
-| G    | 20    |                        |
-| H    | 10    | 5H for 45, 10H for 80  |
-| I    | 35    |                        |
-| J    | 60    |                        |
-| K    | 80    | 2K for 150             |
-| L    | 90    |                        |
-| M    | 15    |                        |
-| N    | 40    | 3N get one M free      |
-| O    | 10    |                        |
-| P    | 50    | 5P for 200             |
-| Q    | 30    | 3Q for 80              |
-| R    | 50    | 3R get one Q free      |
-| S    | 30    |                        |
-| T    | 20    |                        |
-| U    | 40    | 3U get one U free      |
-| V    | 50    | 2V for 90, 3V for 130  |
-| W    | 20    |                        |
-| X    | 90    |                        |
-| Y    | 10    |                        |
-| Z    | 50    |                        |
-+------+-------+------------------------+
-    """
 
-    items = build_items(table_string)
+    with open('price_table.txt') as table_string:
+        items = build_items(table_string)
 
     sku_count = defaultdict(int)
 
@@ -143,9 +113,3 @@ def checkout(skus):
     price_freebies_first = calc_price_freebies_first(items, copy.deepcopy(sku_count))
 
     return min(price_volume_first, price_freebies_first)
-
-
-
-
-
-
